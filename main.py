3d294406d4f57a5a49bf2631513a59e1af869a7e
Yarik21YT <125871440+yarik21yt@users.noreply.github.com>
2025-04-20 21:54:42 +0300

    1.1
    debloat

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ a = random.randint(1, 10)
 def game():
     guess = input.get()
     if int(guess) == int(a):
-        result.config(text="Няяя (≧∇≦) Всё правильно! Ты угадал!", fg="green")
+        result.config(text="Всё правильно! Ты угадал!", fg="green")
     else:
-        result.config(text="Блиин >~<   Ты НЕ угадал! Попробуй ещё раз :3", fg="red")
+        result.config(text="Ты НЕ угадал! Попробуй ещё раз", fg="red")
 
 def new_game():
     global a
@@ -25,13 +25,13 @@ app = tk.Tk()
 app.title("Угадай число :3")
 app.geometry("800x600")
 
-label = tk.Label(app, text="Привееет! >w< Угадай число от 1 до 10!", font=("Arial", 25))
+label = tk.Label(app, text="Привееет! Угадай число от 1 до 10!", font=("Arial", 25))
 label.pack(pady=10)
 
 input = tk.Entry(app)
 input.pack()
 
-button = tk.Button(app, text="Проверить :3", font=("Arial", 20), command=game)
+button = tk.Button(app, text="Проверить", font=("Arial", 20), command=game)
 button.pack(pady=5)
 
 result = tk.Label(app, text="(⁄⁄•⁄ω⁄•⁄⁄)", font=("Arial", 23))
@@ -40,7 +40,7 @@ result.pack()
 new_game_btn = tk.Button(app, text="Новая игра!", font=("Arial", 23), command=new_game)
 new_game_btn.pack()
 
-label = tk.Label(app, text=":3", font=("Arial", 200))
+label = tk.Label(app, text=":D", font=("Arial", 200))
 label.pack(pady=10)
 
 app.mainloop()
